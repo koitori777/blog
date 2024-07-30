d4391e9f0cc31bb42a4dcd661ed2f3f75856650e
koitori777 <sorasahsx@gmail.com>
2024-07-30 20:51:09 +0800

    feat: 添加visitor count badge

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -3,12 +3,27 @@ import "./custom.css";
 import { h } from "vue";
 import { useData } from "vitepress";
 
+const env = import.meta.env;
+
 export default {
   extends: theme,
   Layout: () => {
     const { frontmatter } = useData();
-    return h(theme.Layout, {
-      class: frontmatter.value.layoutClass,
-    });
+    return h(
+      theme.Layout,
+      {
+        class: frontmatter.value.layoutClass,
+      },
+      {
+        "nav-bar-title-after": () =>
+          env.DEV
+            ? ""
+            : h("img", {
+                class: "custom-nav-bar-title-after",
+                src: "https://visitor-badge.laobi.icu/badge?page_id=koitori777",
+                style: "margin-left: 10px;",
+              }),
+      }
+    );
   },
 };
